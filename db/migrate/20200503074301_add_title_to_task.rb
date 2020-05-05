d95c5ea7c0a86de128d130d6b00618717c37c46a
Hiro <xxbbxx825@gmail.com>
2020-05-05 15:16:23 +0900

    production error

--- db/migrate/20200503074301_add_title_to_task.rb
+++ db/migrate/20200503074301_add_title_to_task.rb
@@ -1,4 +1,4 @@
-class AddTitleToTask < ActiveRecord::Migration[5.2]
+class AddTitleToTask < ActiveRecord::Migration[6.0]
   def change
     add_column :tasks, :title, :string
     
--- db/migrate/20200503232545_add_start_time_to_task.rb
+++ db/migrate/20200503232545_add_start_time_to_task.rb
@@ -1,4 +1,4 @@
-class AddStartTimeToTask < ActiveRecord::Migration[5.2]
+class AddStartTimeToTask < ActiveRecord::Migration[6.0]
   def change
     add_column :tasks, :start_time, :datetime
   end
