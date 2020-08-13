75eaf2816201cac6972860a045d876c87ed4f701
Hiro <xxbbxx825@gmail.com>
2020-08-13 23:05:17 +0900

    make model-unit-test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    id {1}
     team_name {"testers"}
     sequence(:email) { |n| "test#{n}@example.com"}
     password {"password"}
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Match, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
-  it "チーム名とメールアドレスとパスワードがあれば登録できる" do 
+  it "タイトルと内容と試合日と開催日があれば投稿できる" do 
     expect(FactoryBot.create(:match)).to be_valid
   end 
 end
